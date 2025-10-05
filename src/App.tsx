fe5887c30ae5bc19d41fdd7313d9c71846c74377
hachizeus <harchijohns01@gmail.com>
2025-10-05 17:07:14 +0300

    Switch to HashRouter to fix SPA routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import { useState, useEffect } from "react";
 import Index from "./pages/Index";
@@ -63,7 +63,7 @@ const App = () => {
             <CookieConsent />
             <PWAInstaller />
             {isLoading && <LoadingScreen />}
-            <BrowserRouter>
+            <HashRouter>
               <ScrollToTop />
               <Analytics />
               <Routes>
@@ -81,7 +81,7 @@ const App = () => {
               <ChatWidget />
               <PricingCalculator />
               <ScrollToTopButton />
-            </BrowserRouter>
+            </HashRouter>
           </TooltipProvider>
         </QueryClientProvider>
     </ErrorBoundary>
